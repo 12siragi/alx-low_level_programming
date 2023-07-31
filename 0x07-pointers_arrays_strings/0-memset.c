4d5afcef295c74fe55f5930a75ea2f555984f7be
12siragi <siragisaidi218@gmail.com>
2023-07-31 11:02:41 -0700

    wawa

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
  * _memset - fill the memory
- * @s - input valule
- * @b - input value
- * @n -input value
+ * @s: input valule
+ * @b: input value
+ * @n: input value
  *
- * return: change array with value
+ * Return: change array with value
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
+
 	for (; n > 0; i++)
 	{
 		s[i] = b;
