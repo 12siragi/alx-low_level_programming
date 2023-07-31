6a16473755ffcec35224a7935ff238d28c366107
12siragi <siragisaidi218@gmail.com>
2023-07-31 13:16:20 -0700

    said

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_chessboard - Entry point
  * @a: array
@@ -13,6 +14,6 @@ void print_chessboard(char (*a)[8])
 	{
 		for (j = 0; j < 8; j++)
 			putchar(a[i][j]);
-		_putchar('\n');
+		putchar('\n');
 	}
 }
