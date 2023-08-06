cec3d51d22173436ef972939a0c993292e826232
12siragi <siragisaidi218@gmail.com>
2023-08-06 02:27:49 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,20 +7,19 @@
  */
 void print_square(int size)
 {
-        if (size <= 0)
-        {
-                putchar('\n');
-        } else
-        {
-                int i, j;
+	if (size <= 0)
+	{
+	putchar('\n')
+	{
+	int i, j;
 
-                for (i = 0; i < size; i++)
-                {
-                        for (j = 0; j < size; j++)
-                        {
-                                putchar('#');
-                        }
-                        putchar('\n');
-                }
-        }
+	for (i = 0; i < size; i++)
+	{
+	for (j = 0; j < size; j++)
+	{
+	putchar('#');
+	}
+	putchar('\n');
+	}
+	}
 }
