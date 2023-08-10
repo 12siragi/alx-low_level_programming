a09e1d89fedb3f77859d3ecf3e1c95fd542cf797
12siragi <siragisaidi218@gmail.com>
2023-08-10 15:42:42 -0700

    wawa

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -50,9 +50,9 @@ void errors(void)
 }
 
 /**
- * main - multiplies two positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - multiplies two positive 
+ * @argc: number 
+ * @argv: array rguments
  *
  * Return: always 0 (Success)
  */
@@ -60,6 +60,7 @@ int main(int argc, char *argv[])
 {
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
@@ -98,7 +99,4 @@ int main(int argc, char *argv[])
 	_putchar('\n');
 	free(result);
 	return (0);
-
-
-
 }
