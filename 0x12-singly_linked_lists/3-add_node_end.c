b1a456004fdf5fc3b183c978c659af2fd4eb2a3b
12siragi <siragisaidi218@gmaol.com>
2023-10-19 17:52:43 -0700

    linked

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,14 +11,14 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new;
+
 	list_t *temp = *head;
 unsigned int len = 0;
 
 	while (str[len])
 	len++;
 
- new = malloc(sizeof(list_t));
+	new = malloc(sizeof(list_t));
 	if (!new)
 	return (NULL);
 
@@ -27,10 +27,10 @@ unsigned int len = 0;
 	new->next = NULL;
 
 	if (*head == NULL)
- {
+{
 	*head = new;
 	return (new);
- }
+}
 
 	while (temp->next)
 	temp = temp->next;
